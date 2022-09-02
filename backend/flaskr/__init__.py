9a1f3546d06ae9bd89a657bc36b073e56d91998c
Johnny Jarecsni <johnny.jarecsni@icloud.com>
2022-09-02 12:33:02 +0100

    Added 400 and 500

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -265,8 +265,10 @@ def create_app(test_config=None):
   Create error handlers for all expected errors 
   including 404 and 422. 
   '''
+  @app.errorhandler(400)
   @app.errorhandler(404)
   @app.errorhandler(422)
+  @app.errorhandler(500)
   def handle_404_error(error):
     return jsonify({
         "success": False,
